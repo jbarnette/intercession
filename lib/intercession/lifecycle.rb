432726319b6fd719e2aa0e763be7e54270d570c4
John Barnette <jbarnette@gmail.com>
2009-01-11 18:06:09 -0800

    Expose controller, request, and response to session.

--- lib/intercession/lifecycle.rb
+++ lib/intercession/lifecycle.rb
@@ -7,10 +7,16 @@ module Intercession
 
     def intercede_before
       session.extend Transient::Session
+
+      session.controller = self if session.respond_to?(:controller=)
+      session.request = request if session.respond_to?(:request=)
+      session.response = response if session.respond_to?(:response=)
+
       session.after_initialize if session.respond_to? :after_initialize
     end
 
     def intercede_after
+      # FIXME: this obviously won't work with after filters in subclasses.
       session.before_save if session.respond_to? :before_save
     end
   end
