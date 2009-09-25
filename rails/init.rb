f375eb8666cb167fe2301ec741e4030c6f51f8c5
W. Andrew Loe III <andrew@andrewloe.com>
2009-09-25 15:03:37 -0700

    Rails 2.3 compatibility.

--- rails/init.rb
+++ rails/init.rb
@@ -1,2 +1,9 @@
-require "application"
+unless defined?(ApplicationController)
+  if ActionPack::VERSION::MINOR >= 3 # Rails 2.3+
+    require "application_controller"
+  else # Rails 2.2 or earlier
+    require "application"
+  end
+end
+
 ApplicationController.send :include, Intercession::Lifecycle
